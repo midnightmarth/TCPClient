108f663de591dec400762ea60d2a7eb15dbe83a7
midnightmarth <midnightmarth@gmail.com>
2019-01-09 13:51:58 -0600

    Removed all dependancies. User can now manually input datato be sent.

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 #!/usr/bin/env node
-const program = require('commander');
 const net = require("net");
 const readline = require ("readline")
 
 const { IpAddress, port } = require("./config");
 const option = { host: IpAddress.IpAddress, port: port.port };
-let rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-program
-  .version('0.0.1')
-  .description('A TCP client that communicates with a server')
-  
-program.parse(process.argv);
 
 let getConn = connName => {
   let client = net.createConnection(option, () => {
@@ -35,91 +24,129 @@ let getConn = connName => {
 
   client.setEncoding("utf8");
 
-  client.on("connect", () => {
-
-    console.log("Got Connection!");
-    connected = true;
-    rl.setPrompt('Enter a command')
-    rl.prompt()
-    rl.on('line', (input) => {
-      input = input.toLowerCase()
-      switch( input !== '' ){
-        case input === 'time':
-          client.write( JSON.stringify({'request': 'time'}) )
-          console.log('Asking for Server Time')
-          break;
-        case input === 'count':
-          client.write(JSON.stringify({'request': 'count'}) )
-          console.log('Asking for Count')
-          break;
-        case input === 'exit':
-          client.end()
-          break;
-        default:
-          return
-      }
-    })
-    rl.prompt()
-  });
-
-  let hb = null;
   client.on("data", (data) => {
     console.log("\n............\nGot Data!: ", data, '\n............\n');
     try {
       let parsed = data.split('\n')
-      for(let i of parsed){
-        if(i !== ''){
-          let incomingData = JSON.parse(i);
-          if(incomingData.type === 'heartbeat' && hb && !incomingData.msg){
-            if (incomingData.epoch == hb.epoch+1 || incomingData.epoch == hb.epoch){
-              hb = incomingData
-            }else{
-              console.log('Data didnt get received in 2 seconds')
-              cleint.end()
+      for(let json of parsed){
+        if(json !== ''){
+          let incomingData = JSON.parse(json);
+          if(incomingData.type === 'msg'){ //checks to see if there is only a message attribute
+            try{
+              if (incomingData.msg.time) {
+                if(incomingData.msg.random > 30){
+                  console.log('Random number is greater than 30: ', incomingData.msg.random)
+                }
             }
-          }else{
-            if(!incomingData.msg)
-              hb = incomingData
+            }catch(err){
+              console.log('Could not parse time object: ', err)
+            }
           }
         }
       }
     } catch (err) {
-      console.log("JSON object not formatted properly");
-      client.end()
+      console.error("*******ERROR******* \nJSON object not formatted properly");
     }
   });
 
+
   client.on("end", function() {
     console.log("Client socket disconnect. ");
-    relogin()
-  });
-
-  client.on("timeout", function() {
-    console.log("Connection timeout.");
-    client.end()
   });
 
   client.on("error", function(err) {
     console.error('An error has occurred: ', JSON.stringify(err));
     client.end()
   });
-
   return client;
+}
+
+const listenForCommand = (rl)=>{
+  rl.prompt();
+  return new Promise((res,rej) => {
+    rl.once('line', (input) => {
+    input = input.toLowerCase();
+    switch( input ){
+      case 'time':
+        res ({'request': 'time'});
+        console.log('Asking for Server Time');
+        break;
+      case 'count':
+        res({'request': 'count'});
+        console.log('Asking for Count');
+        break;
+      case 'exit':
+        rej("exit");
+        break;
+      default:
+        try{
+          let parse = JSON.parse(input)
+          console.log('valid json input')
+          res(parse)
+        }catch(e){
+          res("bad");
+        }
+    }
+    rl.once('close',()=>rej('closed'));
+  });
+});
+}
+
+const login = (client,name) => {
+  client.write(JSON.stringify({ name }));
+}
+
+const connect = (name) => {
+  let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.setPrompt('Enter a command');
+
+  let client = getConn("Node");
+  client.setTimeout(2000);
+  client.once("connect", () => {
+    login(client,name);
+    mainLoop(rl,client);
+  });
+  client.once("timeout", function() {
+    console.log("Connection timeout.");
+    client.end();
+    rl.close();
+    return connect(name);
+  });
 };
 
-const relogin = () => {
-  rl.question('Login: ', (name)=> {
-    if(name.length > 12){
-      console.log('Usernames cannot be longer than 12 characters')
-      relogin();
+const getLogin=()=> {
+  return new Promise((res,rej)=>{
+    let rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+      rl.question('Login: ', (name)=> {
+      if(name.length > 12){
+        rl.close();
+        rej('Usernames cannot be longer than 12 characters');
+      } else {
+        rl.close();
+        res(name)
+      };
+    });
+  });
+}
+
+const mainLoop=(rl,client) => {
+  listenForCommand(rl).then(data=>{
+    if (!client.destroyed) {
+      if (data!=='bad')
+        client.write(JSON.stringify(data));
+      mainLoop(rl,client);
     }
-    login(name)
+  }).catch((e)=>{
+    console.error("*** Catch in mainloop.....")
   })
 }
 
-const login = (name) => {
-  var nodeClient = getConn("Node");
-  nodeClient.write(JSON.stringify({ name }));
-};
-
-relogin()
+getLogin().then(name=>{
+  connect(name);
+});
